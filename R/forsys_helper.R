915852cd711f9a0755cd59775fc8b6eac8312248
Cody <cevers@pdx.edu>
2022-06-17 11:55:21 -0700

    changed threshold operator message to ">="

--- R/forsys_helper.R
+++ R/forsys_helper.R
@@ -11,7 +11,7 @@
 #' 
 forsys_helper_prep_threshold <- function(stand_threshold){
   if(!is.null(stand_threshold)){
-    message('PatchMax assumes project stand threshold operator is ">"')
+    message('PatchMax assumes project stand threshold operator is ">="')
     x <- stand_threshold %>%
       stringr::str_split('>=|<=|>|<|==', simplify = T)
     if(length(x) != 2){
